d9d4febcdfc18389683ec480af1297bf6a9fbdee
AlessandroSpallina <alessandrospallina1@gmail.com>
2023-08-28 18:47:58 +0000

    PoC

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SentenceTransformersServer
-API Server for SentenceTransformers embedders 
+
+This project implements an API server for SentenceTransformers' embedders which aims to be a drop-in replacement for [llama-cpp-python's webserver embeddings](https://github.com/abetlen/llama-cpp-python#web-server).
+
+## Why should I use it?
+If you are using the llama-cpp-python webserver and you are experiencing poor embedding performance, now you can try another embedder without modifying your client code!
+
+
+## Quickstart
+
+Clone the project
+```bash
+git clone https://github.com/AlessandroSpallina/SentenceTransformersServer.git
+```
+
+Choose the model you want to use for the embedding, [here](https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models/) all the supported models from SentenceTransformers.
+
+Keep in mind that the right model to pick mostly depends on your use case and your data, so try some similarity search in order to understand which model fits better your needs. For example you can start by comparing [all-MiniLM-L6-v2](https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2) and [all-mpnet-base-v2](https://huggingface.co/sentence-transformers/all-mpnet-base-v2).
+
+When you are done, modify the model name in the .env file. If you are behind a corporate proxy remember to uncomment the right section in the docker-compose.yml file.
+
+Then impose your hands on the keyboard, close your eyes and 
+```bash
+docker compose up
+```
---
+++ server/main.py
@@ -0,0 +1,59 @@
+import os
+from typing import List
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+from typing import Union
+from sentence_transformers import SentenceTransformer
+
+app = FastAPI(title="SentenceTransformersServer")
+model = SentenceTransformer(os.environ['MODEL'], cache_folder='/code/server/models')
+
+
+# Enable CORS
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
+class EmbeddingRequest(BaseModel):
+    input: Union[str, List[str]] = Field(description="The input to embed.")
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "input": "The food was delicious and the waiter...",
+                }
+            ]
+        }
+    }
+
+
+@app.get("/")
+async def read_root():
+    return {"status": "up"}
+
+@app.post("/v1/embeddings")
+async def create_embeddings(request: EmbeddingRequest):
+    embeddings = model.encode(request.input).tolist()
+
+    if type(embeddings[0]) == list:
+        to_ret = [{
+            'object': 'embedding',
+            'embedding': embedding,
+            'index': i
+        } for i, embedding in enumerate(embeddings)]
+    else:
+        to_ret = [{
+            'object': 'embedding',
+            'embedding': embeddings,
+            'index': 0
+        }]
+
+    return {
+        'object': 'list',
+        'data': to_ret,
+        'model': os.environ['MODEL'],
+        'usage': { # If someone is using this field we should find the right way to fill these values
+            'prompt_tokens': len(request.input),
+            'total_tokens': len(request.input)
+        }
+    }

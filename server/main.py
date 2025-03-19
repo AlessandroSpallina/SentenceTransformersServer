e85dbc8b13a255ef227260cde0472de72c823b83
AlessandroSpallina <alessandrospallina1@gmail.com>
2025-03-19 12:58:17 +0100

    better parallelism

--- server/main.py
+++ server/main.py
@@ -7,9 +7,9 @@ from typing import Union
 from sentence_transformers import SentenceTransformer
 import logging
 import sys
-
-app = FastAPI(title="SentenceTransformersServer")
-model = SentenceTransformer(os.environ['MODEL'], cache_folder='/code/server/models', device="cuda")
+import os
+# import asyncio
+# import concurrent.futures
 
 logFormatter = logging.Formatter("%(asctime)s - [%(levelname)s] %(name)s [%(module)s.%(funcName)s:%(lineno)d]: %(message)s")
 console_handler = logging.StreamHandler(stream=sys.stdout)
@@ -19,8 +19,20 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(console_handler)
 
+
+logger.info(f"Loading model {os.environ['MODEL']}")
+model = SentenceTransformer(os.environ['MODEL'], cache_folder='/code/server/models')
+# pool = model.start_multi_process_pool()#['cpu'] * os.cpu_count() * int(0.75))
+
+# process_pool = concurrent.futures.ProcessPoolExecutor()
+
+logger.info("Starting fastapi server")
+
+app = FastAPI(title="SentenceTransformersServer")
+
 # Enable CORS
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
 
 class EmbeddingRequest(BaseModel):
     input: Union[str, List[str]] = Field(description="The input to embed.")
@@ -42,12 +54,15 @@ async def read_root():
 
 
 @app.post("/v1/embeddings")
-async def create_embeddings(request: EmbeddingRequest):
+def create_embeddings(request: EmbeddingRequest):
     logger.info(f"Received {request}")
 
     # model.encode() doens't support strings with only space, below a workaround
     if request.input == ' ':
         request.input = '.'
+
+    # embeddings = model.encode_multi_process(request.input, pool)
+
 
     embeddings = model.encode(request.input).tolist()
 
